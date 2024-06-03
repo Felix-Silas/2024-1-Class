ea6052c0b790e1661f1b798217a97d3418621bd2
GeunTae You <ygeuntae1208@naver.com>
2024-06-03 12:01:53 +0900

    fix: 타자 게임 time 측정 시간 수정

--- Industrial System Programming/15A-Taza_game.py
+++ Industrial System Programming/15A-Taza_game.py
@@ -6,13 +6,13 @@ problem = ['lion', 'tiger', 'dragon', 'cat', 'dog', 'monkey']
 n = 1
 print('타자게임 할 준비가 되면 엔터를 눌러주세요!')
 input()
+start = time.time()
 
 
 while n <= 5:
     print('문제', n)
     q = random.choice(problem)
     print(q, '를 따라 입력하세요!')
-    start = time.time()
     a = input()
 
     if q == a:
@@ -25,5 +25,5 @@ while n <= 5:
 end = time.time()
 et = end - start
 
-print(f' 소요된 시간은 {et:0.2f}초입니다.')
+print(f'소요된 시간은 {et:0.2f}초입니다.')
 
